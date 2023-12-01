c18e983eb94270db740aa2bf9fa4e2dafb40fef4
Sean Moulton <seanmoulton@my.unt.edu>
2023-12-01 02:06:26 -0600

    Cleaned up and simplified code greatly
    Removed the redundant Maybe's from getFirstNum and getClosest.
    
    Moved getClosest into getFirstNum since it only really makes sense in
    the context of that function.
    
    Switched to using a match statement for getClosest instead of an if/else.
    
    Removed strToNum in favor of numberMap. This makes things a lot easier
    since we already know what the associated Char should be for any given
    number we want to search with.
    
    Switched names of variables to more descriptive ones.

--- Day 1/Main.hs
+++ Day 1/Main.hs
@@ -5,90 +5,57 @@ import Data.List
 part1 :: IO ()
 part1 = do
     contents <- readFile "input.txt"
-    let lin = lines contents
-    let f = map (\x -> fromJust $ find isDigit x) lin
-    let l = map (\x -> fromJust $ find isDigit $ reverse x) lin
-    let n = map (\(a, b) -> read [a,b] :: Int) (zip f l)
-    let s = sum n
-    print s
+    let lines' = lines contents
+    let firstDigits = map (\x -> fromJust $ find isDigit x) lines'
+    let lastDigits = map (\x -> fromJust $ find isDigit $ reverse x) lines'
+    let numbers = zipWith (\a b -> read [a, b] :: Int) firstDigits lastDigits
+    let sum' = sum numbers
+    print sum'
 
-strToNum :: String -> Maybe Char
-strToNum "one"   = Just '1'
-strToNum "two"   = Just '2'
-strToNum "three" = Just '3'
-strToNum "four"  = Just '4'
-strToNum "five"  = Just '5'
-strToNum "six"   = Just '6'
-strToNum "seven" = Just '7'
-strToNum "eight" = Just '8'
-strToNum "nine"  = Just '9'
-strToNum "eno"   = Just '1'
-strToNum "owt"   = Just '2'
-strToNum "eerht" = Just '3'
-strToNum "ruof"  = Just '4'
-strToNum "evif"  = Just '5'
-strToNum "xis"   = Just '6'
-strToNum "neves" = Just '7'
-strToNum "thgie" = Just '8'
-strToNum "enin"  = Just '9'
-strToNum "1" = Just '1'
-strToNum "2" = Just '2'
-strToNum "3" = Just '3'
-strToNum "4" = Just '4'
-strToNum "5" = Just '5'
-strToNum "6" = Just '6'
-strToNum "7" = Just '7'
-strToNum "8" = Just '8'
-strToNum "9" = Just '9'
-strToNum _ = Nothing
-
-numStrs :: [String]
-numStrs =
-        [ "one"
-        , "two"
-        , "three" 
-        , "four"  
-        , "five"  
-        , "six"   
-        , "seven" 
-        , "eight" 
-        , "nine"
-        , "1"
-        , "2"
-        , "3"
-        , "4"
-        , "5"
-        , "6"
-        , "7"
-        , "8"
-        , "9"
-        ]
-
-numStrsRev :: [String]
-numStrsRev = map reverse numStrs
+numberMap :: [(String, Char)]
+numberMap =
+    [ ("one",   '1')
+    , ("two",   '2')
+    , ("three", '3')
+    , ("four",  '4')
+    , ("five",  '5')
+    , ("six",   '6')
+    , ("seven", '7')
+    , ("eight", '8')
+    , ("nine",  '9')
+    , ("1", '1')
+    , ("2", '2')
+    , ("3", '3')
+    , ("4", '4')
+    , ("5", '5')
+    , ("6", '6')
+    , ("7", '7')
+    , ("8", '8')
+    , ("9", '9')
+    ]
 
 findString :: (Eq a) => [a] -> [a] -> Maybe Int
-findString search str = findIndex (isPrefixOf search) (tails str)
+findString token str = findIndex (isPrefixOf token) (tails str)
 
-getClosest :: (Maybe Int, Maybe Char) -> (Int, Maybe Char) -> (Int, Maybe Char)
-getClosest (sI, sV) (accI, accV)
-    | isJust sI && fromJust sI < accI = (fromJust sI, sV)
-    | otherwise = (accI, accV)
-
-getFirstNum :: String -> [String] -> Char
-getFirstNum xs predicates =
-    let (i, v) = foldr (\x acc -> getClosest (findString x xs, strToNum x) acc) (length xs, Nothing) predicates
-    in fromJust v
+getFirstNum :: String -> [(String, Char)] -> (String -> String) -> Char
+getFirstNum xs tokens fn =
+    let (i, v) = foldl (\acc (x, c) -> getClosest (findString (fn x) (fn xs), c) acc) (length xs, Nothing) tokens
+    in fromMaybe '0' v
+    where
+        getClosest :: (Maybe Int, Char) -> (Int, Maybe Char) -> (Int, Maybe Char)
+        getClosest (foundIdx, foundNumAsChar) (accI, accV)
+            | fromMaybe (maxBound :: Int) foundIdx < accI = (fromJust foundIdx, Just foundNumAsChar)
+            | otherwise = (accI, accV)
 
 part2 :: IO ()
 part2 = do
     contents <- readFile "input.txt"
-    let lins = lines contents
-    let f = map (\x -> getFirstNum x numStrs) lins
-    let l = map (\x -> getFirstNum (reverse x) numStrsRev) lins
-    let n = map (\(a, b) -> read [a,b] :: Int) (zip f l)
-    let s = sum n
-    print s
+    let lines' = lines contents
+    let firstDigits = map (\x -> getFirstNum x numberMap id) lines'
+    let lastDigits = map (\x -> getFirstNum x numberMap reverse) lines'
+    let numbers = zipWith (\a b -> read [a,b] :: Int) firstDigits lastDigits
+    let sum' = sum numbers
+    print sum'
 
 main :: IO ()
 main = do

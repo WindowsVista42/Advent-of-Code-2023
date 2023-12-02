b893b85f5c55341d0f412243a2b40b300987f269
Sean Moulton <seanmoulton@my.unt.edu>
2023-12-02 12:46:26 -0600

    Update to use case statement and general cleanup

--- Day2/Main.hs
+++ Day2/Main.hs
@@ -2,11 +2,15 @@ module Main where
 
 import Data.Maybe
 
--- Part 1
+-- Shared
+
+whenMaybe :: Bool -> a -> Maybe a
+whenMaybe False _ = Nothing
+whenMaybe True a = Just a
 
 split :: Char -> String -> [String]
 split c xs = case break (==c) xs of 
-  (ls, "") -> [ls]
+  (ls, [])   -> [ls]
   (ls, _:rs) -> ls : split c rs
 
 cleanupOutput :: String -> String
@@ -15,30 +19,25 @@ cleanupOutput xs = drop 5 $ filter (\x -> x/=':' && x/=';' && x/=',') xs
 splitOutput :: String -> [String]
 splitOutput = split ' '
 
+-- Part 1
+
 isValid :: [String] -> Bool
 isValid [] = True
 isValid [_] = True
 isValid (x:y:xs) =
     let n = read x :: Int
-    in
-        if y == "red" then
-            n <= 12 && isValid xs
-        else if y == "green" then
-            n <= 13 && isValid xs
-        else if y == "blue" then
-            n <= 14 && isValid xs
-        else
-            isValid xs
+    in case y of
+        "red"   -> n <= 12 && isValid xs
+        "green" -> n <= 13 && isValid xs
+        "blue"  -> n <= 14 && isValid xs
+        _       -> isValid xs
 
 isPossible :: String -> Maybe Int
 isPossible line =
     let xs = splitOutput $ cleanupOutput line
         num = read $ head xs :: Int
-    in
-        if isValid (tail xs) then
-            Just num
-        else
-            Nothing
+    in 
+        whenMaybe (isValid $ tail xs) num
 
 part1 :: IO ()
 part1 = do
@@ -50,24 +49,25 @@ part1 = do
 
 -- Part 2
 
-doTheThing :: (Int, Int, Int) -> [String] -> (Int, Int, Int)
-doTheThing acc [] = acc
-doTheThing acc [_] = acc
-doTheThing (r, g, b) (x:y:xs) =
+getTotalPower :: (Int, Int, Int) -> [String] -> (Int, Int, Int)
+getTotalPower acc [] = acc
+getTotalPower acc [_] = acc
+getTotalPower (r, g, b) (x:y:xs) =
     let n = read x :: Int
-    in
-        if y == "red" then
-            doTheThing (max r n , g, b) xs
-        else if y == "green" then
-            doTheThing (r, max g n, b) xs
-        else if y == "blue" then
-            doTheThing (r, g, max b n) xs
-        else
-            doTheThing (r, g, b) xs
+    in case y of
+        "red"   -> getTotalPower (max r n, g, b) xs
+        "green" -> getTotalPower (r, max g n, b) xs
+        "blue"  -> getTotalPower (r, g, max b n) xs
+        _       -> getTotalPower (r, g, b) xs
+
+totalPower :: [String] -> (Int, Int, Int)
+totalPower = getTotalPower (0, 0, 0)
 
 calcPower :: String -> Int
 calcPower line = 
-    let (r,g,b) = doTheThing (0, 0, 0) $ tail $ splitOutput $ cleanupOutput line in r * g * b
+    let (r,g,b) = totalPower $ tail $ splitOutput $ cleanupOutput line
+    in
+        r * g * b
 
 part2 :: IO ()
 part2 = do

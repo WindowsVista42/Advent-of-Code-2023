693837e38bcfb25490c17758ba03ff90425c5487
Sean Moulton <seanmoulton@my.unt.edu>
2023-12-05 19:23:38 -0600

    Cleanup, simplification, and code deduplication

--- Day5/Main.hs
+++ Day5/Main.hs
@@ -33,9 +33,7 @@ findInfoMapping :: V.Vector InfoMapping -> Int -> Int
 findInfoMapping m srcVal =
     foldl foldFn srcVal m where
         foldFn acc srcFoldVal =
-            let dstMap = dstMapping srcFoldVal
-                srcMap = srcMapping srcFoldVal
-                range' = range srcFoldVal
+            let InfoMapping dstMap srcMap range' = srcFoldVal
             in
                 if srcVal >= srcMap && srcVal < (srcMap + range') then
                     dstMap + (srcVal - srcMap)
@@ -76,6 +74,11 @@ parseInfoMapping xs =
             in
                 (V.fromList infoMappings, rhs)
 
+getPairs :: [Int] -> [(Int,Int)]
+getPairs [] = []
+getPairs [_] = []
+getPairs (x:y:xs) = (x,y) : getPairs xs
+
 parseInput :: [T.Text] -> SeedMaps
 parseInput lines' =
     let (seeds',l0)                 = parseSeeds lines'
@@ -97,60 +100,30 @@ parseInput lines' =
             temperatureToHumidity'
             humidityToLocation'
 
-part1 :: SeedMaps -> Int
-part1 m =
-    let seeds' = seeds m
-        s0 = seedToSoil m
-        s1 = soilToFertilizer m
-        s2 = fertilizerToWater m
-        s3 = waterToLight m
-        s4 = lightToTemperature m
-        s5 = temperatureToHumidity m
-        s6 = humidityToLocation m
+getMinLocation :: SeedMaps -> Int -> Int -> Int
+getMinLocation seedMap acc x =
+    let SeedMaps _ m0 m1 m2 m3 m4 m5 m6 = seedMap
+        sv0 = findInfoMapping m0 x
+        sv1 = findInfoMapping m1 sv0
+        sv2 = findInfoMapping m2 sv1
+        sv3 = findInfoMapping m3 sv2
+        sv4 = findInfoMapping m4 sv3
+        sv5 = findInfoMapping m5 sv4
+        sv6 = findInfoMapping m6 sv5
     in
-        foldl (foldFn (s0,s1,s2,s3,s4,s5,s6)) maxBound seeds' where
-            foldFn (s0,s1,s2,s3,s4,s5,s6) acc x =
-                let sv0 = findInfoMapping s0 x
-                    sv1 = findInfoMapping s1 sv0
-                    sv2 = findInfoMapping s2 sv1
-                    sv3 = findInfoMapping s3 sv2
-                    sv4 = findInfoMapping s4 sv3
-                    sv5 = findInfoMapping s5 sv4
-                    sv6 = findInfoMapping s6 sv5
-                in
-                    min sv6 acc
+        min sv6 acc
 
-getPairs :: [Int] -> [(Int,Int)]
-getPairs [] = []
-getPairs [_] = []
-getPairs (x:y:xs) = (x,y) : getPairs xs
+part1 :: SeedMaps -> Int
+part1 seedMaps =
+    foldl (getMinLocation seedMaps) maxBound (seeds seedMaps)
 
 part2 :: SeedMaps -> Int
-part2 m =
-    let seeds' = seeds m
-        s0 = seedToSoil m
-        s1 = soilToFertilizer m
-        s2 = fertilizerToWater m
-        s3 = waterToLight m
-        s4 = lightToTemperature m
-        s5 = temperatureToHumidity m
-        s6 = humidityToLocation m
+part2 seedMaps =
+    let seedPairs = V.fromList $ getPairs $ V.toList $ seeds seedMaps
     in
-        let seedPairs = V.fromList $ getPairs $ V.toList seeds'
-        in
-            V.foldl' (foldFn (s0,s1,s2,s3,s4,s5,s6)) maxBound seedPairs where
-                foldFn (s0,s1,s2,s3,s4,s5,s6) acc (start', range') =
-                    V.foldl' (foldFn' (s0,s1,s2,s3,s4,s5,s6)) acc (V.fromList [start'..start'+range']) where
-                        foldFn' (s0',s1',s2',s3',s4',s5',s6') acc' x =
-                            let sv0 = findInfoMapping s0' x
-                                sv1 = findInfoMapping s1' sv0
-                                sv2 = findInfoMapping s2' sv1
-                                sv3 = findInfoMapping s3' sv2
-                                sv4 = findInfoMapping s4' sv3
-                                sv5 = findInfoMapping s5' sv4
-                                sv6 = findInfoMapping s6' sv5
-                            in
-                                min sv6 acc'
+        V.foldl' foldFn maxBound seedPairs where
+            foldFn acc (start', range') =
+                V.foldl' (getMinLocation seedMaps) acc (V.fromList [start'..start'+range'])
 
 main :: IO ()
 main = do
